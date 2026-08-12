e8f2f5de28b4816e1e7e92b4627a28d4771e727a
viclito <berglin1998@gmail.com>
2026-08-12 20:21:18 +0530

    restructure 2

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    optimizeCss: true,
+  },
   images: {
-    unoptimized: true,
+    unoptimized: false,
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
