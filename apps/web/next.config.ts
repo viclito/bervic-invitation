fcecd60c4e6e6e66acd82dc28d26779d4d31ca73
viclito <berglin1998@gmail.com>
2026-08-26 12:52:28 +0530

    implement expo

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    optimizeCss: true,
-  },
   images: {
     unoptimized: false,
     formats: ["image/avif", "image/webp"],
